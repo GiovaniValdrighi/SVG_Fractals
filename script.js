d1c58028eec15b4cf5cba18336bbe777571927d3
giovai valdrighi <giovani.valdrighi@gmail.com>
2019-10-21 23:43:42 -0300

    created method do delete circles

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var svg = document.getElementById("inline");
+var zoom_center = 205;
+var ativo = 0;
+var txt_info = document.getElementById("info");
+function iniciar(){
+    ativo = 1;
+}
 
-function circle_obj(id, r, x, y, scale){
-    this.id = id;
-    this.r = r;
-    this.x = x;
-    this.y = y;
-    this.scale = scale;
+function pausar(){
+    ativo = 0;
 }
 
 function create_circle(id, r, x, y){
@@ -20,38 +22,85 @@ function create_circle(id, r, x, y){
     svg.appendChild(c);
 }
 
+//array of the ids
 var circle_ar = new Array();
-var min_r = 10000;
-var depth = 0;
-function generate_circles(r, x ,y){
-    min_r = r < min_r ? r : min_r;
-    circle_ar.push(new circle_obj("c"+depth, r, x, y, 1));
-    create_circle("c"+depth,r, x, y);
-    depth = circle_ar.length;
+var ind = 0;
+function generate_circles(depth, r, x ,y){
+
+    //creating circle
+    create_circle("c-"+depth+ "-"+ind, r, x ,y);
+
+    //recursion update
+    if( circle_ar[depth]){
+        circle_ar[depth].push("c-" + depth + "-" + ind);
+    }else{
+        circle_ar[depth] = new Array();
+        circle_ar[depth].push("c-" + depth + "-" + ind);
+    }
+    depth++;
+    ind++;
     if (r > 2){
-        generate_circles(r/2, x-r, y);
-        generate_circles(r/2, x+r, y);
+        generate_circles(depth, r/2, x-r, y);
+        generate_circles(depth, r/2, x+r, y);
     }
 }
 
 
-generate_circles(70, 150, 150);
-console.log(min_r);
-console.log(depth);
+function generate_circles_loop(depth, r, x, y){
+    
+    //creating circle
+    create_circle("c-"+depth+ "-"+ind, r, x ,y);
+
+    //recursion update
+    if( circle_ar[depth]){
+        circle_ar[depth].push("c-" + depth + "-" + ind);
+    }else{
+        circle_ar[depth] = new Array();
+        circle_ar[depth].push("c-" + depth + "-" + ind);
+    }
+    depth++;
+    ind++;
+    if (r > 2){
+        generate_circles(depth, r/2, x-r, y);
+        generate_circles(depth, r/2, x+r, y);
+    }
+}
+
+generate_circles(0, 70, 150, 150);
+depth = circle_ar.length;
+n_remov = 0;
+
 
 function update_scale(){
-    for(let i = 0; i < circle_ar.length; i++){
-        c = document.getElementById("c" + i);
-        x = parseFloat(c.getAttributeNS(null, "cx"));
-        //mudando o raio
-        r = parseFloat(c.getAttributeNS(null, "r"));
-        c.setAttributeNS(null, "r", r + r/10);
-        //mudando o x
-        dist = Math.abs((150 - x)/r);
-        if (x > 150){
-            c.setAttributeNS(null, "cx", x + dist*r/10);
-        }else if (x < 150){
-            c.setAttributeNS(null, "cx", x - dist*r/10);
+    txt_info.innerHTML = "Depth:" + depth + "&emsp; Circles: " + ind;
+    if(ativo == 1){
+        for(let i = 0; i< depth; i++){
+            for(let j = 0; j< circle_ar[i].length; j++){
+                c = document.getElementById(circle_ar[i][j]);
+                r = parseFloat(c.getAttributeNS(null, "r"));
+                x = parseFloat(c.getAttributeNS(null, "cx"));
+                c.setAttributeNS(null, "r", r + r/22);
+                c.setAttributeNS(null, "cx", x + (x-zoom_center)/22);
+            }
+        }
+
+        c_min = document.getElementById(circle_ar[circle_ar.length-1][0]);
+        r_min = parseFloat(c_min.getAttributeNS(null, "r"));
+        if(r_min >2){
+            circle_ar[circle_ar.length] = new Array();
+            for(let i = 0; i < circle_ar[circle_ar.length-2].length; i++){
+                c = document.getElementById(circle_ar[circle_ar.length-2][i]);
+                x = parseFloat(c.getAttributeNS(null, "cx"));
+                if( (x - r_min > 0) && (x + r_min < 300)){
+                    ind++;
+                    circle_ar[circle_ar.length-1].push("c-"+depth+"-"+ind);
+                    create_circle("c-"+depth+"-"+ind, r_min/2, x - r_min, 150);
+                    ind++;
+                    circle_ar[circle_ar.length-1].push("c-"+depth+"-"+ind);
+                    create_circle("c-"+depth+"-"+ind, r_min/2, x + r_min, 150);
+                }
+            }
+            depth = circle_ar.length;
         }
     }
 }
